98616e796f74c0838b3713ded4fe5c14701c709c
guan99716 <guan99716@gmail.com>
2026-08-26 23:51:55 +0800

    0826

---
+++ 594. Longest Harmonious Subsequence.cpp
@@ -0,0 +1,22 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> um;
+        int maxLen = 0;
+        for(int n: nums){
+            um[n]++;
+        }
+        for(auto &[key,val] : um){
+            if(um.count(key-1))
+                maxLen = max(maxLen, um[key]+um[key-1]);
+            if(um.count(key+1))
+                maxLen = max(maxLen, um[key]+um[key+1]);
+        }
+        return maxLen;
+        
+    }
+};
--- 643. Maximum Average Subarray I.cpp
+++ 643. Maximum Average Subarray I.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 class Solution {
 public:
-    double findMaxAverage(vector<int>& nums, int k) {
-        
-
-
-
-
-
-
-        
+    double findMaxAverage(vector<int> &nums, int k) {
+        int n = nums.size();
+        double sum = 0;
+        for (int i = 0; i < k; i++)
+            sum += nums[i];
+        double maxSum = sum;
+        for (int i = 0; i < n - k; i++) {
+            sum -= nums[i];
+            sum += nums[i + k];
+            maxSum = max(maxSum, sum);
+        }
+        return maxSum;
     }
 };
